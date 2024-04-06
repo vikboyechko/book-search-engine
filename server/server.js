30735d7bd67f973909462cee6033f19f2c5ecc05
Vik Boyechko <vik.boyechko@gmail.com>
2024-04-06 18:29:07 -0500

    delete controller and routes folders

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 // import apollo server and middlware
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes'); don't need this anymore
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +25,9 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server, {
+      context: authMiddleware
+    }));
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
